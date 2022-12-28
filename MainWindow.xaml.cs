00d54c8b6b869567f56d85b5e5d9f8232b65f1ff
Nikilesh05 <nikinikilesh1@gmail.com>
2022-12-28 11:36:38 +0530

    For loop

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace For_loop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+       
+
+
+        
+private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder Print = new StringBuilder();
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    for (int k = 0; k < 4; k++)
+                    {
+                        for (int l = 0; l < 5; l++)
+                        {
+                            Print.Append(i.ToString());
+                            Print.Append(j.ToString());
+                            Print.Append(k.ToString());
+                            Print.Append(l.ToString());
+                            Print.AppendLine(",");
+                            lbltxt.Content = Print.ToString();
+                        }
+                    }
+                }
+            }
+            
+            
+        }
+    }
+}
